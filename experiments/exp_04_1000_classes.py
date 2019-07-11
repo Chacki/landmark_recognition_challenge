dcb71e4275e7c9efd4d1692b3bd5762bc59aef10
Praktikum 2019 <p19g1@i14s37.anthropomatik.kit.edu>
2019-07-11 10:00:07 +0200

    Fixed optimizer and input pipeline

--- experiments/exp_04_1000_classes.py
+++ experiments/exp_04_1000_classes.py
@@ -3,20 +3,20 @@ File: exp_00_test.py
 Description: Experiment script for testing purpose only
 """
 import ignite
-import torch
 import numpy as np
+import pandas as pd
+import torch
 from absl import app, flags
 from PIL import Image
-from torchvision import transforms
-from torch.nn.modules.loss import CrossEntropyLoss
-import config
-from dataset import landmark_recognition
-from utils import evaluation, logging, data
-import models
 from torch import nn
-import pandas as pd
+from torch.nn.modules.loss import CrossEntropyLoss
 from torch.utils.data import DataLoader
+from torchvision import transforms
 
+import config
+import models
+from dataset import landmark_recognition
+from utils import data, evaluation, logging
 
 flags.DEFINE_float("lr", 0.001, "Learning rate")
 flags.DEFINE_integer("num_classes", 1000, "Number of Classes")
@@ -25,16 +25,13 @@ FLAGS = flags.FLAGS
 
 
 class ClassSampler(torch.utils.data.Sampler):
-
     def __init__(self, data_source):
-        self.first_1000_classes = np.reshape(np.argwhere(data_source < FLAGS.num_classes),(-1,))
-
-
-
+        self.first_1000_classes = np.reshape(
+            np.argwhere(data_source < FLAGS.num_classes), (-1,)
+        )
 
     def __iter__(self):
         return iter(np.random.permutation(self.first_1000_classes))
-
 
     def __len__(self):
         return len(self.first_1000_classes)
@@ -47,49 +44,52 @@ def main(_):
     label_encoder = data.LabelEncoder()
     config.init_experiment()
 
-    df_train = pd.read_csv("./data/google-landmark/train.csv")
+    df_train = pd.read_csv("./data/google-landmark/valid_train.csv")
 
     transform = transforms.Compose(
         [
-            transforms.Lambda(Image.open),
-            transforms.Grayscale(3),
-            transforms.Resize(size=(FLAGS.height, FLAGS.width)),
+            transforms.Lambda(lambda x: Image.open(x).convert("RGB")),
+            transforms.RandomCrop(224, pad_if_needed=True),
             transforms.ToTensor(),
-            transforms.Normalize(
-                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-            ),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         ]
     )
-    df_train["landmark_id"] = label_encoder.fit_transform(df_train["landmark_id"].to_numpy())
+    encoded_labels = label_encoder.fit_transform(
+        df_train["landmark_id"].to_numpy()
+    )
+    df_train["landmark_id"] = encoded_labels
     dataset = landmark_recognition.Dataset(
         df_train, "./data/google-landmark/train", transform
     )
-
+    sampler = ClassSampler(encoded_labels)
     dataloader = DataLoader(
         dataset=dataset,
         batch_size=FLAGS.batch_size,
-        sampler=ClassSampler,
-        num_workers=16
+        sampler=sampler,
+        num_workers=16,
     )
-
-
 
     model = models.build_model()
-    optimizer = torch.optim.Adam(model.parameters(), lr=FLAGS.lr)
-    trainer = ignite.engine.create_supervised_trainer(
-        model=model,
-        optimizer=optimizer,
-        loss_fn=CrossEntropyLoss(),
-        device=FLAGS.device,
-        non_blocking=True,
-    )
-    if FLAGS.eval is False:
-        model.fc = nn.Linear(model.fc.in_features, FLAGS.num_classes)
-        #evaluater = evaluation.build_evaluater(model, test_loader)
-        #evaluation.attach_eval(evaluater, trainer, train_loader)
-        #logging.attach_loggers(trainer, evaluater, model)
-    trainer.run(dataloader, max_epochs=100)
-
+    if FLAGS.eval:
+        print("eval")
+    else:
+        train_model = nn.Sequential(
+            model,
+            nn.ReLU(),
+            nn.Linear(model.fc.out_features, FLAGS.num_classes),
+        )
+        optimizer = torch.optim.Adam(train_model.parameters(), lr=FLAGS.lr)
+        trainer = ignite.engine.create_supervised_trainer(
+            model=train_model,
+            optimizer=optimizer,
+            loss_fn=CrossEntropyLoss(),
+            device=FLAGS.device,
+            non_blocking=True,
+        )
+        logging.attach_loggers(
+            train_engine=trainer, eval_engine=None, model=model
+        )
+        trainer.run(dataloader, max_epochs=100)
 
 
 if __name__ == "__main__":
